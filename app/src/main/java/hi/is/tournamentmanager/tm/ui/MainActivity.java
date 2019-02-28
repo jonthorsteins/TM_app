f010354ac9c6a61f08c8eb173a8c417ef1ffb4ba
Birgir Snorrason <birgir.snorrason@gmail.com>
2019-02-28 14:56:48 +0000

    Komið basic útlit og virkni á sign in activity.

--- app/src/main/java/hi/is/tournamentmanager/tm/ui/MainActivity.java
+++ app/src/main/java/hi/is/tournamentmanager/tm/ui/MainActivity.java
@@ -1,15 +1,54 @@
 package hi.is.tournamentmanager.tm.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import hi.is.tournamentmanager.tm.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mSignInButton;
+    // private static final int REQUEST_CODE_VIEWALLTOURNAMENTS = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSignInButton = (Button) findViewById(R.id.signIn_button);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean verifyLogin = true; // kalla á eitthvað verification fall
+                if (verifyLogin) {
+                    Intent i = ViewAllTournamentsActivity.newIntent(MainActivity.this, true);
+                    // startActivityForResult(i, REQUEST_CODE_VIEWALLTOURNAMENTS);
+                    startActivity(i);
+                    finish();
+                } else {
+                    LayoutInflater inflater = getLayoutInflater();
+                    View layout = inflater.inflate(R.layout.custom_toast,
+                            (ViewGroup) findViewById(R.id.custom_toast_container));
+
+                    TextView text = (TextView) layout.findViewById(R.id.text);
+                    text.setText(R.string.login_failed);
+
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setGravity( Gravity.BOTTOM, 0, 50);
+                    toast.setDuration(Toast.LENGTH_LONG);
+                    toast.setView(layout);
+                    toast.show();
+                }
+            }
+        });
+
     }
 }
