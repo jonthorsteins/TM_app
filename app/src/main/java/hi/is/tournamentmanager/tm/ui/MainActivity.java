3669c455f996bdce14d7ffd7b6408dfa9f34161b
Kristján Petur Þórarinsson <kth130@hi.is>
2019-02-28 15:39:12 +0000

    Network handler settur upp

--- app/src/main/java/hi/is/tournamentmanager/tm/ui/MainActivity.java
+++ app/src/main/java/hi/is/tournamentmanager/tm/ui/MainActivity.java
@@ -1,15 +1,18 @@
 package hi.is.tournamentmanager.tm.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import hi.is.tournamentmanager.tm.R;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String baseUrl = "http://localhost:8080";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/hi/is/tournamentmanager/tm/ui/ViewAllTournamentsActivity.java
+++ app/src/main/java/hi/is/tournamentmanager/tm/ui/ViewAllTournamentsActivity.java
@@ -3,7 +3,14 @@ package hi.is.tournamentmanager.tm.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 import hi.is.tournamentmanager.tm.R;
+import hi.is.tournamentmanager.tm.helpers.NetworkHandler;
 
 public class ViewAllTournamentsActivity extends AppCompatActivity {
 
@@ -11,5 +18,17 @@ public class ViewAllTournamentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_tournaments);
+
+        NetworkHandler.get("/tournaments",  null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                System.out.println(response.toString());
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                System.out.println(response.toString());
+
+            }
+        });
     }
 }
