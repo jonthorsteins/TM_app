36947b104ea7d1059ffa0c387446e787a133ded7
kmg14 <35534095+kmg14@users.noreply.github.com>
2019-03-11 11:48:58 +0000

    View á profile lagað

--- app/src/main/java/hi/is/tournamentmanager/tm/ui/ViewProfileActivity.java
+++ app/src/main/java/hi/is/tournamentmanager/tm/ui/ViewProfileActivity.java
@@ -1,15 +1,46 @@
 package hi.is.tournamentmanager.tm.ui;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 import hi.is.tournamentmanager.tm.R;
+import hi.is.tournamentmanager.tm.helpers.Mapper;
+import hi.is.tournamentmanager.tm.helpers.NetworkHandler;
+import hi.is.tournamentmanager.tm.model.Tournament;
 
-public class ViewProfileActivity extends AppCompatActivity {
+public class ViewProfileActivity extends ListActivity {
+
+    // private static
+    private ListView mListView;
+
+    private static final String EXTRA_MESSAGE = "is.hi.tournamentmanager.mainToAll";
+    private static final String TOURNAMENT_ITEM = "TOURNAMENT_ITEM";
+    private List<Tournament> mTournaments;
+    TextView mViewAll;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,7 +56,7 @@ public class ViewProfileActivity extends AppCompatActivity {
                     finish();
                     return true;
                 case R.id.view:
-                    i = ViewAllTournamentsActivity.newIntent(ViewProfileActivity.this, true);
+                    i = ViewProfileActivity.newIntent(ViewProfileActivity.this, true);
                     // startActivityForResult(i, REQUEST_CODE_VIEWALLTOURNAMENTS);
                     startActivity(i);
                     finish();
@@ -41,12 +72,94 @@ public class ViewProfileActivity extends AppCompatActivity {
         }
     };
 
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, ViewProfileActivity.class);
+        // i.putExtra(EXTRA_MESSAGE, answerIsTrue);
+        return i;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+        final Context ct = getApplicationContext();
+
+
+        NetworkHandler.get("/tournaments",  null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                System.out.println(response.toString());
+
+                try{
+                    mTournaments = Mapper.mapToTournamentList(response.getJSONArray("tournaments"));
+                    TournamentArrayAdapter adapter = new TournamentArrayAdapter(ct, mTournaments);
+                    setListAdapter(adapter);
+
+                } catch (JSONException e){
+                    Log.e("Mapper", e.toString());
+                }
+            }
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                System.out.println(response.toString());
+                mTournaments = Mapper.mapToTournamentList(response);
+                TournamentArrayAdapter adapter = new TournamentArrayAdapter(ct, mTournaments);
+                setListAdapter(adapter);
+            }
+        });
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+        // TODO Auto-generated method stub
+        super.onListItemClick(l, v, position, id);
+
+        Intent intent = new Intent(this, ViewTournamentActivity.class);
+        intent.putExtra(TOURNAMENT_ITEM, mTournaments.get(position));
+        startActivity(intent);
+    }
+
+    public class TournamentArrayAdapter extends ArrayAdapter<Tournament>  {
+        private final Context context;
+        private final List<Tournament> values;
+
+        public TournamentArrayAdapter(Context context, List<Tournament> values) {
+            super(context, R.layout.tournament_list_row, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.tournament_list_row, parent, false);
+            Tournament t = values.get(position);
+            TextView textView = (TextView) rowView.findViewById(R.id.tournament_name);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            textView.setText(t.getName());
+
+
+            textView = (TextView) rowView.findViewById(R.id.tournament_status);
+            Date currentDate = new Date();
+            if (t.getMatches().size() != 0) {
+                textView.setText("Started");
+                textView.setTextColor(Color.parseColor("#000000"));
+            } else if (t.getSignUpExpiration() == null ||
+                    t.getSignUpExpiration().compareTo(currentDate) < 0) {
+                textView.setText("Open");
+                textView.setTextColor(Color.parseColor("#00FF00"));
+            } else {
+                textView.setText("Closed");
+                textView.setTextColor(Color.parseColor("#FF00"));
+            }
+            // Change the icon for Windows and iPhone
+
+            return rowView;
+        }
     }
 }
